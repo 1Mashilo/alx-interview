f2b227b080237646baf69ad144d2fa5e46aea712
1Mashilo <christophermashilo@gmail.com>
2024-07-25 12:23:07 +0200

    UTF8 validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,43 @@
+ #!/usr/bin/python3
+
+"""
+Module for validating UTF-8 encoded data.
+"""
+
+
+def valid_utf8(data):
+    """
+    Determines if a given data set represents a valid UTF-8 encoding.
+
+    Args:
+        data: A list of integers representing bytes of data.
+
+    Returns:
+        True if the data is valid UTF-8, False otherwise.
+    """
+
+    num_bytes = 0
+    mask1 = 1 << 7  # 10000000
+    mask2 = 1 << 6  # 01000000
+
+    for byte in data:
+        mask = 1 << 7
+
+        if num_bytes == 0:
+            while mask & byte:
+                num_bytes += 1
+                mask = mask >> 1
+
+            if num_bytes == 0:
+                continue
+
+            if num_bytes == 1 or num_bytes > 4:
+                return False
+
+        else:
+            if not (byte & mask1 and not (byte & mask2)):
+                return False
+
+        num_bytes -= 1
+
+    return num_bytes == 0
